a9376da45e94a1a02336c0be580cdfd7810f2847
tHALL3000 <80265634+tHALL3000@users.noreply.github.com>
2021-06-27 16:43:45 -0600

    Created dynamically changing fields

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import ScotchInfoBar from "./ScotchInfoBar";
 
 function App() {
-  // MOST OF YOUR CODE GOES HERE
+  const [name, setName] = useState("Name");
+  const [age, setAge] = useState("0");
 
   return (
     <div className="App">
@@ -14,8 +15,8 @@ function App() {
 
       {/* Display Data */}
       <div className="input-display">
-        <p>Display Name: </p>
-        <p>Display Age: </p>
+        <h3>Display Name: {name} </h3>
+        <h3>Display Age: {age} </h3>
       </div>
 
       {/* Collect User Inputs */}
@@ -23,13 +24,25 @@ function App() {
         {/* Input name */}
         <div className="field">
           <label className="label">Name: </label>
-          <input className="input" type="text" placeholder="William" />
+          <input
+            className="input"
+            type="text"
+            value={name}
+            placeholder="William"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         {/* Input age */}
         <div className="field">
           <label className="label">Age: </label>
-          <input className="input" type="number" placeholder="38" />
+          <input
+            className="input"
+            type="number"
+            value={age}
+            placeholder="38"
+            onChange={(e) => setAge(e.target.value)}
+          />
         </div>
       </div>
       <ScotchInfoBar seriesNumber="3" />
